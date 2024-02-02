c021ca7941a05f1a26717e68329200c48fe50d5b
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-02 11:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 let btn = document.getElementByid("btn");
-let text = document.getElementByid("text");
+let text = document.getElementById("text")
 let count = 0;
 
 function handleToIncreament(){
 	count++;
-	window.alert(count);
+	// window.alert(count);
 	text.innerHTML = count;
 }
