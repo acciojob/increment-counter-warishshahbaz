3da141e768234fe62d6de1ada2dcb08eaea12d40
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-02 11:17:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn = document.getElementByid("btn");
+let text = document.getElementByid("text");
+let count = 0;
+
+function handleToIncreament(){
+	count++;
+	text.innerHtml = count;
+}
